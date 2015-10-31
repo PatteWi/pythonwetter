120e21b52109abc46e374022c4c18c6ef97d8a3a
Zwergchief <patte.wilhelm@googlemail.com>
2015-10-31 12:00:09 +0100

    Tests

--- pythonwetter/tests.py
+++ pythonwetter/tests.py
@@ -9,3 +9,15 @@ class TestAvailability(unittest.TestCase):
     def testServer(self):
         response = self.Client.get('/weather/')
         self.assertEqual(response.status_code, 200)
+
+    def testWeather(self):
+        response = self.Client.get('/weathers/')
+        self.assertEqual(response.status_code, 200)
+
+class TestAvailabilityAgain(unittest.TestCase):
+    def setUp(self):
+        self.Client = Client()
+
+    def testServer(self):
+        response = self.Client.get('/weatheskdhvisdvr/')
+        self.assertEqual(response.status_code, 404)
