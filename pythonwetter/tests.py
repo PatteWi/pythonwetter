59c3f36a2a1fa4976e7272784076d6a2de812e59
Zwergchief <patte.wilhelm@googlemail.com>
2015-11-30 11:21:24 +0100

    Added tests

--- pythonwetter/tests.py
+++ pythonwetter/tests.py
@@ -1,5 +1,6 @@
-import unittest, doctest
+import unittest
 
+from urllib.request import urlopen
 from django.test import Client
 from pythonwetter.functions import stadtidy
 
@@ -20,9 +21,14 @@ class TestServerAvailability(unittest.TestCase):
         response = self.Client.get('/weather/?city=Brandenburg+an+der+Havel')
         self.assertEqual(response.status_code, 200)
 
+    def testS3Available(self):
+        response = urlopen('https://s3.amazonaws.com/pythonwetter/static/images/wc/d_0_L.png')
+        self.assertEqual(response.getcode(), 200)
+
 
 class TestFunktions(unittest.TestCase):
 
     def testYahooStadtID(self):
         woe = stadtidy('Potsdam')
         self.assertEqual(woe, '685783')
+
