3fd0908e62cb39a418acac7979eeb790cb35c0e4
Zwergchief <patte.wilhelm@googlemail.com>
2015-12-02 14:28:41 +0100

    Removed unused files

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(name='django 1.7 on Red Hat Openshift',
-    version='0.1',
-    description='django on OpenShift',
-    author='Your Name',
-    author_email='example@example.com',
-    url='https://github.com/jfmatth/openshift-django17',
-)
