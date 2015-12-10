83ad44af036ab84e44bbc7301df467da0c225246
Zwergchief <patte.wilhelm@googlemail.com>
2015-12-10 18:57:28 +0100

    Autodeploy to EBS

--- pythonwetter/settings.py
+++ pythonwetter/settings.py
@@ -30,7 +30,7 @@ TEMPLATE_DEBUG = True
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': TEMPLATE_DIRS,
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
